5ecb4729dd68653f28fe331daba8a9a4ce0ab119
Kevin Schäfers <schaefers@math.uni-wuppertal.de>
2024-03-11 10:31:17 +0100

    Update update.h
    changes in comment block

--- include/update.h
+++ include/update.h
@@ -4,6 +4,7 @@
 * File update.h
 *
 * Copyright (C) 2011, 2017, 2018 Martin Luescher
+* 2024 Kevin Schaefers, Jacob Finkenrath
 *
 * This software is distributed under the terms of the GNU General Public
 * License (GPL)
